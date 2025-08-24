6b7025070c59fc95781dfc2befd302ee3acca5d4
Amit Gold <amit.goldshtein2@gmail.com>
2025-08-24 17:48:59 +0300

    New timestamp to test cicd pipeline

--- part3/docker/app.py
+++ part3/docker/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     config_message = os.getenv("CONFIG_MESSAGE", "Welcome to flask")
     api_key = os.getenv("API_KEY", "No API Key")
-    return f"{config_message} - API Key: {api_key}, VERSION TIMESTAMP 21-08-2025 16:51 TEST"
+    return f"{config_message} - API Key: {api_key}, VERSION TIMESTAMP 24-08-2025 17:48 TEST"
     
 @app.route("/health/live")
 def liveness():
